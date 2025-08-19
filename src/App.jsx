1cf0d6263f861aada865c62057cb3b550a6b42e9
Ernesto Martinez Escobar <gagamart@Gabriels-MacBook-Pro.local>
2025-08-18 20:06:56 -0600

    primer clase

---
+++ src/App.jsx
@@ -0,0 +1,98 @@
+//import "./App.css";
+import React from "react";
+import { useState } from "react";
+
+// hooks
+// built-in hooks
+// useState
+// useEffect
+// custom hooks
+// manually hooks
+
+// Smart Components
+// Stateful components
+// Class Components
+// React 18
+
+// Dumb Components
+// Stateless Components
+// Function Components
+
+/*
+function App() {
+  return <div>Hola Mundo</div>;
+}
+  */
+
+/*
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+    };
+
+    //this.updateName = this.updateName.bind(this);
+  }
+
+  updateName = () => {
+    this.setState({
+      name: "Gabriel",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.updateName}>Click me</button>
+        <span>{"Hola mundo: " + this.state.name}</span>
+      </div>
+    );
+  }
+}
+  */
+
+function App() {
+  // const myState = useState('')
+  //const name = myState[0];
+  //const seState = myState[1];
+
+  // destructuring
+  // hook
+  const [name, setName] = useState("");
+  const [lastname, setLastName] = useState("");
+
+  function updateName() {
+    setName("Gabriel");
+  }
+
+  function updateLastName() {
+    setLastName("Martinez");
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          updateName();
+          updateLastName();
+        }}
+      >
+        Click me
+      </button>
+      <span>{"Hola mundo: " + name}</span>
+      <LastName lastname={lastname} updateLastName={updateLastName} />
+    </div>
+  );
+}
+
+function LastName(props) {
+  return (
+    <>
+      <button onClick={props.updateLastName}>Click me</button>
+      <div>{props.lastname}</div>
+    </>
+  );
+}
+
+export default App;
